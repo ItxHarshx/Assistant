cf2eca73ba9695ec77565c2a392dbf6b18ce40c3
ItxHarshx <itxharshx@gmail.com>
2026-06-26 13:01:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1208,18 +1208,18 @@ async def promote(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     custom_title=admin_title
                 )
         
-        except Exception:
-            pass
+        except Exception as e:
+            print(f"TITLE ERROR: {e}")
             
             text = (
                 f"👮 <b>User Promoted</b>\n\n"
                 f"👤 User: {target.mention_html()}\n"
-                )
+            )
             
             if admin_title:
                 text += f"🏷️ Title: <code>{admin_title}</code>\n"
-                
                 text += f"🛡️ By: {update.effective_user.mention_html()}"
+                
                 await update.message.reply_html(text)
 
 
